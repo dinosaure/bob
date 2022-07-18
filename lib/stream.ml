be3e0cc44d76bb0335a4ad45de821098d519cc85
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-07-18 15:19:43 +0200

    Fill license about stream.ml{,i}

--- lib/stream.ml
+++ lib/stream.ml
@@ -1,4 +1,5 @@
 (* Copyright (c) 2020 Rizo I. <rizo@odis.io>
+   Copyright (c) 2022 Romain Calascibetta <romain.calascibetta@gmail.com>
 
    Permission to use, copy, modify, and distribute this software for any
    purpose with or without fee is hereby granted, provided that the above
