b9b011c9e4aaf50985fd27c9d01ddec30a6911d9
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-08-14 10:45:25 +0200

    Handle the new identity event and show up into the CLI
    This patch factorize also the way to process packets. Currently, a
    TCP/IP packet can have multiple Bob's packets. We try to parse all of
    them and aggregate events if we go them (`Identity and `Agreement).
    According to our protocol, we can not have multiple `Identity or
    multiple `Agreement and `Identity can not appear with `Agreement (in
    this last case, we raise an error).
    
    The idea is to show up the identity given by the relay to the sender and
    the receiver can confirm the reception if the identity of the sender
    matches with what the real sender gave to it.
    
    We ensure that the relay can **not** generate the same identity for 2
    active peers.

--- lib/bob_unix.mli
+++ lib/bob_unix.mli
@@ -47,6 +47,7 @@ module Make (IO : IO) : sig
     IO.fd ->
     ?reproduce:bool ->
     g:Random.State.t ->
+    ?identity:(string -> unit Fiber.t) ->
     Spoke.secret ->
     ( string * (Spoke.cipher * Spoke.cipher) * Spoke.shared_keys,
       [> error ] )
@@ -61,6 +62,7 @@ module Make (IO : IO) : sig
     ?reproduce:bool ->
     choose:(string -> [ `Accept | `Refuse ] Fiber.t) ->
     g:Random.State.t ->
+    ?identity:(string -> unit Fiber.t) ->
     string ->
     ( string * (Spoke.cipher * Spoke.cipher) * Spoke.shared_keys,
       [> error ] )
@@ -72,12 +74,13 @@ module Make (IO : IO) : sig
       accept or refuse the peer found. *)
 
   val relay :
+    g:Random.State.t ->
     ?timeout:float ->
     Unix.file_descr ->
     Bob.Secured.t ->
     stop:unit Fiber.Ivar.t ->
     unit Fiber.t
-  (** [relay ?timeout socket room ~stop] launch a relay which can accept
+  (** [relay ~g ?timeout socket room ~stop] launch a relay which can accept
       connections from [socket]. The user can specify a [timeout] value (how
       long the relay can keep an active connection with a peer). If [stop] is
       filled, the relay terminates. The given [room] is filled by peers which
