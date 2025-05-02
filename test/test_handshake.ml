edcfc42dcca11e01ce7d5e54eff6717c188a8c8b
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-05-02 12:27:32 +0200

    Lint tests and how we compile them

--- test/test_handshake.ml
+++ test/test_handshake.ml
@@ -1,26 +1,3 @@
-let () = Printexc.record_backtrace true
-
-let reporter ppf =
-  let report src level ~over k msgf =
-    let k _ =
-      over ();
-      k ()
-    in
-    let with_metadata header _tags k ppf fmt =
-      Format.kfprintf k ppf
-        ("%a[%a]: " ^^ fmt ^^ "\n%!")
-        Logs_fmt.pp_header (level, header)
-        Fmt.(styled `Magenta string)
-        (Logs.Src.name src)
-    in
-    msgf @@ fun ?header ?tags fmt -> with_metadata header tags k ppf fmt
-  in
-  { Logs.report }
-
-let () = Fmt_tty.setup_std_outputs ~style_renderer:`Ansi_tty ~utf_8:true ()
-let () = Logs.set_reporter (reporter Fmt.stdout)
-let () = Logs.set_level ~all:true (Some Logs.Debug)
-
 let process_packet data =
   let ctx = Bob.Handshake.make () in
   let rec go data acc = function
